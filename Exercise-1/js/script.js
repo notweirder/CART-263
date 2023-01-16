8eef39291fe3cb2e4101a3a6310f4376a59b71be
notweirder <merulzalot@gmail.com>
2023-01-15 20:17:10 -0500

    Pong game

--- Exercise-1/js/script.js
+++ Exercise-1/js/script.js
@@ -20,10 +20,34 @@ function preload() {
 /**
 Description of setup
 */
+
+let timer = 0;
+let freq = 1000;
+
+let P1Score = 0;
+let P2Score = 0;
+
+let paddleWidth = 10;
+let paddleHeight = 100;
+let paddleSpeed = 10
+
+
+let paddle1X = 20;
+let paddle1Y = 0;
+let paddle2X = 20;
+let paddle2Y = 0;
+
+let ballX = 100;
+let ballY = 10;
+let ballXAccel = 3;
+let ballYAccel = 3;
+let speedChange = -1.1;
+
+
 function setup() {
 
 createCanvas(500,500);
-background(0,255,0)
+ paddle2X = width-paddleWidth-paddle2X;
 
 }
 
@@ -33,4 +57,116 @@ Description of draw()
 */
 function draw() {
 
+    background(0,255,0)
+  
+    fill(0)
+    text(paddle1X,paddle1X,paddle1Y)
+    text(paddle2X,paddle2X,paddle2Y)
+    rect(paddle1X,paddle1Y,paddleWidth,paddleHeight)
+    rect(paddle2X,paddle2Y,paddleWidth,paddleHeight)
+    if (millis()>timer)
+    {
+        text(ballX,ballX,ballY)
+        square(ballX,ballY,20)
+        keyChecker()
+         ballChecker()
+   }
+   
+   pointsDisplay();
+
+
+}
+
+
+
+function keyChecker()
+{
+
+    if (keyIsDown(UP_ARROW))
+    {
+        paddle1Y-=paddleSpeed;
+    }
+    if (keyIsDown(DOWN_ARROW))
+    {
+        paddle1Y+=paddleSpeed;
+    }
+    if (keyIsDown(87))
+    {
+        paddle2Y-=paddleSpeed;
+    }
+    if (keyIsDown(83))
+    {
+        paddle2Y+=paddleSpeed;
+    }
+}
+function ballChecker()
+{
+    if (ballY<=0)
+    {
+    ballYAccel*=speedChange;
+    }
+    if (ballY>height)
+    {
+        ballYAccel*=speedChange;
+    }
+    if (ballX<=paddle1X && (ballY>paddle1Y && ballY < paddle1Y+paddleHeight ))
+    {
+        ballXAccel*=speedChange;
+        ballYAccel*=speedChange;
+    }
+    else if (ballY<paddle1Y || ballY > paddle1Y+paddleHeight){
+    }
+    if (ballX>=paddle2X && (ballY>paddle2Y && ballY < paddle2Y+paddleHeight))
+    {
+        ballXAccel*=speedChange;
+        ballYAccel*=speedChange;
+    }
+    if (ballX<0)
+    {
+        P2Score++;
+        ballX=width/2;
+        ballY=height/2;
+        speedChange=1;
+        timer = freq + millis();
+    }
+     if (ballX>width)
+    {
+        P1Score++
+        ballX=width/2;
+        ballY=height/2;
+        speedChange=1;
+        timer = freq + millis();
+    }
+    
+    ballX=ballX+ballXAccel;
+    ballY=ballY+ballYAccel;
+
+
+}
+function pointsDisplay()
+{
+    noFill()
+    for (let i = 1; i<=10;i++)
+    {
+        square(i*20,50,10)
+    }
+
+    noFill()
+    for (let i = 1; i<=10;i++)
+    {
+        square(width-i*20,50,10)
+    }
+
+    fill(0)
+    for (let i = 1; i<=P1Score;i++)
+    {
+        square(i*20,50,10)
+    }
+
+    fill(0)
+    for (let i = 1; i<=P2Score;i++)
+    {
+        square(width-i*20,50,10)
+    }
+    
 }
